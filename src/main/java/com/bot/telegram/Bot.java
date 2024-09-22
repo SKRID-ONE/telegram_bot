05c2889782bc4f5b58f78afb30a664147fa9dcbe
SKRID_ONE <goodfriend483@gmail.com>
2024-09-22 11:55:42 +0300

    Добавлено: - создание меню коммнад бота - обновлено pom до последней версии tg

--- src/main/java/com/bot/telegram/Bot.java
+++ src/main/java/com/bot/telegram/Bot.java
@@ -3,33 +3,53 @@ package com.bot.telegram;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.meta.TelegramBotsApi;
 import org.telegram.telegrambots.meta.api.methods.AnswerCallbackQuery;
+import org.telegram.telegrambots.meta.api.methods.BotApiMethod;
 import org.telegram.telegrambots.meta.api.methods.CopyMessage;
+import org.telegram.telegrambots.meta.api.methods.commands.SetMyCommands;
+import org.telegram.telegrambots.meta.api.methods.menubutton.SetChatMenuButton;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.methods.send.SendPhoto;
 import org.telegram.telegrambots.meta.api.methods.updatingmessages.EditMessageReplyMarkup;
 import org.telegram.telegrambots.meta.api.methods.updatingmessages.EditMessageText;
 import org.telegram.telegrambots.meta.api.objects.CallbackQuery;
+import org.telegram.telegrambots.meta.api.objects.InputFile;
 import org.telegram.telegrambots.meta.api.objects.Message;
 import org.telegram.telegrambots.meta.api.objects.Update;
-import org.telegram.telegrambots.meta.api.objects.User;
+import org.telegram.telegrambots.meta.api.objects.commands.BotCommand;
+import org.telegram.telegrambots.meta.api.objects.commands.scope.BotCommandScopeChat;
+import org.telegram.telegrambots.meta.api.objects.menubutton.MenuButtonCommands;
 import org.telegram.telegrambots.meta.api.objects.replykeyboard.InlineKeyboardMarkup;
 import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.InlineKeyboardButton;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
 
-import javax.validation.UnexpectedTypeException;
+import java.io.Serializable;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.List;
-import java.util.Locale;
 
 public class Bot extends TelegramLongPollingBot {
+
+    //Для методов использующих Update
+    private Update updateEvent = new Update();
 
     //Переменные
     private boolean screaming = false; //Переменная для выбора режима ответа бота: Крик/тихо
 
+    private long idGPT = 5815596965L;
+
     //Клавиатуры (меню)
+
     private InlineKeyboardMarkup keyboardM1;
     private InlineKeyboardMarkup keyboardM2;
+    private InlineKeyboardMarkup startkeyboard;
 
     //Кнопки
+    InlineKeyboardButton menu = InlineKeyboardButton.builder()
+            .text("Menu")           // Название кнопки
+            .callbackData("menu")   // Данные отпраляемые в update
+            .build();
     InlineKeyboardButton next = InlineKeyboardButton.builder()
             .text("Next")           // Название кнопки
             .callbackData("next")   // Данные отпраляемые в update
@@ -65,13 +85,18 @@ public class Bot extends TelegramLongPollingBot {
     public void onUpdateReceived(Update update) {
         System.out.println(update);
 
+        updateEvent = update;
+
         //Клавиатуры
+        startkeyboard = InlineKeyboardMarkup.builder()
+                .keyboardRow(List.of(menu))
+                .build();
+
         keyboardM1 = InlineKeyboardMarkup.builder()
                 .keyboardRow(List.of(next))
                 .build();
         keyboardM2 = InlineKeyboardMarkup.builder()
-                .keyboardRow(List.of(back))
-                .keyboardRow(List.of(url))
+                .keyboardRow(List.of(back, url))
                 .build();
 
         // Если боту отправили сообщение
@@ -81,15 +106,31 @@ public class Bot extends TelegramLongPollingBot {
             var msg = update.getMessage();
             var user = msg.getFrom();
             var id = user.getId();
-            //Предполагается, что мы знаем команды бота
-            if(msg.getText().equals("/scream")){
-                screaming = true;
-            } else if (msg.getText().equals("/whisper")) {
-                screaming = false;
-            } else if (msg.getText().equals("/menu")){
-                sendMenu(id,"<b>Menu 1</b>", keyboardM1);
-            }
 
+            //Создание коммандного меню
+            createCommands("/start", "Давай начнем всё сначала", "/space", "Взгляни за горизонт событий", "/menu", "Показать меню", "/gpt", "Написать GPT");
+
+            if (msg.isCommand()) {
+                //Предполагается, что мы знаем команды бота
+                if (msg.getText().equals("/scream")) {
+                    screaming = true;
+                } else if (msg.getText().equals("/whisper")) {
+                    screaming = false;
+                } else if (msg.getText().equals("/menu")) {
+                    sendMenu(id, "<b>Menu 1</b>", keyboardM1);
+                } else if (msg.getText().equals("/space")){
+                    System.out.println("SPACE");
+                    sendPhoto(id, "C:\\SKRID.jpg");
+                } else if (msg.getText().equals("/start")){
+                    System.out.println("Start");
+                    startMenu(id);
+                } else if (msg.getText().equals("/gpt")) {
+                    System.out.println("Отправлено сообщение GPT");
+                    sendText(idGPT, "/start");
+                }
+
+                return;             //Выход из метода, т.к. повторять команды - не нужно.
+            }
 
             //Логика поведения бота
             if(screaming){
@@ -98,14 +139,17 @@ public class Bot extends TelegramLongPollingBot {
                 copyMessage(id, msg.getMessageId());
             }
 
-            return; //Мы не хотим повторять команды - выходим
+            //Если получили сообщение от idGPT
+            if (msg.getFrom().getId() == idGPT) {
+                System.out.println("**************************************");
+                System.out.println(msg.getText());
+            }
 
-            //Если нажали на кнопку
-        } else if (update.hasCallbackQuery()) {
-//            System.out.println("CALLBACK");
+        } else if (update.hasCallbackQuery()) {              //Если нажали на кнопку
             //CallBack запрос
             CallbackQuery callbackQuery = update.getCallbackQuery();
 
+            //Данные CallBack запроса
             Long id = callbackQuery.getMessage().getChatId();
             String queryId = callbackQuery.getId();
             String data = callbackQuery.getData();
@@ -113,21 +157,7 @@ public class Bot extends TelegramLongPollingBot {
 
             //Метод для обработки нажатия кнопок
             buttonTap(id, queryId, data, msgId);
-
-//            AnswerCallbackQuery close = AnswerCallbackQuery.builder()
-//                    .callbackQueryId(update.getCallbackQuery().getId()).build();
-//
-//            try {
-//                execute(close);
-//            } catch (TelegramApiException e) {
-//                throw new RuntimeException(e);
-//            }
-
         }
-
-
-
-
     }
 
     @Override
@@ -140,7 +170,7 @@ public class Bot extends TelegramLongPollingBot {
     //Методы
 
     /**
-     * Отправка сообщения
+     * Отправка текстового сообщения сообщения
      * @param who кому
      * @param what что
      */
@@ -150,6 +180,28 @@ public class Bot extends TelegramLongPollingBot {
                 .text(what).build();    //Message content
         try {
             execute(sm);                        //Actually sending the message
+        } catch (TelegramApiException e) {
+            throw new RuntimeException(e);      //Any error will be printed here
+        }
+    }
+
+    /**
+     * Отправка фотографии пользователю
+     * @param who кому
+     * @param paths Путь к файлу
+     */
+    private void sendPhoto(Long who, String paths){
+
+        Path path = Paths.get(paths);
+
+        InputFile file = new InputFile(path.toFile());
+
+        SendPhoto sendPhoto = SendPhoto.builder()
+                .chatId(who.toString())
+                .photo(file).build();
+
+        try {
+            execute(sendPhoto);                 //Actually sending the message
         } catch (TelegramApiException e) {
             throw new RuntimeException(e);      //Any error will be printed here
         }
@@ -169,6 +221,24 @@ public class Bot extends TelegramLongPollingBot {
 
         try {
            execute(cm);
+        } catch (TelegramApiException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    /**
+     * Метод показывающий стартовое меню (по команде /start)
+     * @param who id пользователя для отправки
+     */
+    public void startMenu (Long who) {
+        SendMessage sm = SendMessage.builder()
+                .chatId(who.toString())
+                .text("Приветствую тебя пользователь! Это самый луший telegram bot про космос! \nНажми кнопку меню, чтобы начать путешествие!")
+                .replyMarkup(startkeyboard)
+                .build();
+
+        try {
+            execute(sm);
         } catch (TelegramApiException e) {
             throw new RuntimeException(e);
         }
@@ -202,6 +272,26 @@ public class Bot extends TelegramLongPollingBot {
             execute(sm);
         } catch (TelegramApiException e) {
             throw new RuntimeException(e);
+        }
+    }
+
+    /**
+     * Отправка сообщения с вариантами ответа
+     * @param who кому
+     * @param what сообщение
+     * @param kb клавиатура с вариантами ответа
+     */
+    public void sendTextWithKeyboard (Long who, String what, InlineKeyboardMarkup kb ) {
+        SendMessage sm = SendMessage.builder()
+                .chatId(who.toString())
+                .text(what)
+                .replyMarkup(kb)
+                .build();
+
+        try {
+            execute(sm);                        //Actually sending the message
+        } catch (TelegramApiException e) {
+            throw new RuntimeException(e);      //Any error will be printed here
         }
     }
 
@@ -243,13 +333,79 @@ public class Bot extends TelegramLongPollingBot {
         }
     }
 
+    /**
+     * Метод возвращает ID текущего Telegram-чата
+     */
+    public Long getCurrentChatId() {
+
+
+        if (updateEvent.hasMessage()) {
+            return updateEvent.getMessage().getFrom().getId();
+        }
+
+        if (updateEvent.hasCallbackQuery()) {
+            return updateEvent.getCallbackQuery().getFrom().getId();
+        }
+
+        return null;
+    }
+
+    /**
+     * Метод для создания списка команд для бота
+     * @param commands команды написанные в формате ( "/command", "Название/значение для пользователя" )
+     */
+    private void createCommands (String ... commands) {
+        //Преобразование входных параметров (commands) в ArrayList BotCommands
+        ArrayList<BotCommand> listCommands = new ArrayList<BotCommand>();
+
+        for (int i = 0; i<commands.length; i+=2) {
+            String key = commands [i];
+            String description = commands [i+1];
+
+            if (key.startsWith("/")) //Удаляем  "/"
+                key = key.substring(1);
+
+            BotCommand bc = new BotCommand(key, description);
+
+            listCommands.add(bc);
+        }
+        var chId = getCurrentChatId();
+
+        SetMyCommands cmds = new SetMyCommands();
+        cmds.setCommands(listCommands);
+        cmds.setScope(BotCommandScopeChat.builder().chatId(chId).build());
+        executeTelegramApiMethod(cmds);
+
+        //Показать кнопки меню
+        var ex = new SetChatMenuButton();
+        ex.setChatId(chId);
+        ex.setMenuButton(MenuButtonCommands.builder().build());
+        executeTelegramApiMethod(ex);
+
+    }
+
+
+    /**
+     * Вместо стандартного execute(). Метод обеспечивает типобезопасное выполнение методов Telegram API и обработку возможных исключений.
+     * @param method который необходимо выполнить
+     * @return
+     * @param <T>
+     * @param <Method>
+     */
+    private <T extends Serializable, Method extends BotApiMethod<T>> T executeTelegramApiMethod(Method method) {
+        try {
+            return super.sendApiMethod(method);
+        } catch (TelegramApiException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
     //Основной поток (метод)
     public static void main(String[] args) throws TelegramApiException {
         TelegramBotsApi botsApi = new TelegramBotsApi(DefaultBotSession.class);
         Bot bot = new Bot();
         botsApi.registerBot(bot);
         bot.sendText(Long.valueOf(782908610L), "The SKRID_ONE_BOT is running!");  //The L just turns the Integer into a Long
-
     }
 
 }
